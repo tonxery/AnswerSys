9bf17e7ec22671600cf5d02d05179d39c3e5c433
tonxery <tonxery@gmail.com>
2019-12-10 13:05:32 +0800

    二次更新

--- bin/start.py
+++ bin/start.py
@@ -4,6 +4,7 @@ import os,sys
 BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 #开始程序
+#二次更新
 
 from core.models_v2 import * #反序列化对象,必须保证类已经在内存中
 from core import src
