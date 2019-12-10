b398677798a2777facd0851e52dae630ff74c8bf
tonxery <tonxery@gmail.com>
2019-12-10 12:24:27 +0800

    添加注释

--- bin/start.py
+++ bin/start.py
@@ -3,7 +3,7 @@ __author__ = 'Linhaifeng'
 import os,sys
 BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-
+#开始程序
 
 from core.models_v2 import * #反序列化对象,必须保证类已经在内存中
 from core import src
